515036aff80388ea042109e31ebaa528ef95ed31
ESTUDIANTE <ESTUDIANTE@PC7_LAB1>
2024-01-19 21:12:01 -0500

    aaa

---
+++ ExamenApiRest/Controllers/LibroController.cs
@@ -0,0 +1,42 @@
+﻿using ExamenApiRest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamenApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        private static List<Libro> _libro = new List<Libro>
+        {
+            new Libro
+            {
+                Id = 1,
+                Title = "The Great Gatsby",
+                Author = "F. Scott Fitzgerald",
+                
+            },
+            new Libro
+            {
+                Id = 2,
+                Title = "Cien años de soledad",
+                Author = "Gabriel García Márquez",
+                
+            },
+            new Libro
+            {
+                Id = 3,
+                Title = "1984",
+                Author = "Geoge Orwell",
+              
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Libro> GetAllBooks()
+        {
+            return _libro;
+        }
+    }
+}
---
+++ ExamenApiRest/Models/Libro.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamenApiRest.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+    }
+}
